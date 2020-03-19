1ecf59f6cea54405861f96a36c33ca362017d1fc
lwhlng <hoanglogg@gmail.com>
2020-03-19 15:49:31 +0700

    3 files modified, 2 added

---
+++ src/app/MockData.ts
@@ -0,0 +1,6 @@
+import { Product } from 'Product';
+export const data: Product[] = [
+  {id: 1, name: 'Item 1', price: 24.99, desc: 'Mo ta san pham 1'},
+  {id: 2, name: 'Item 2', price: 24.99, desc: 'Mo ta san pham 2'},
+  {id: 3, name: 'Item 3', price: 24.99, desc: 'Mo ta san pham 3'},
+]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { data } from '../MockData';
+import { Product } from '../Product';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products = data;
+  selected : Product;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.products);
   }
-
+  
+  detailsProduct(product){
+    this.selected = product;
+    console.log(this.selected);
+  }
 }
